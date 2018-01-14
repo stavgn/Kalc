42c86085d37f6f67c4f4d4a93bcfa2c36dd70a7c
stav name <stavgn@gmail.com>
2018-01-14 23:30:08 +0200

    fix dist server

--- tools/distServer.js
+++ tools/distServer.js
@@ -4,12 +4,7 @@ var app = express();
 
 var port = process.env.PORT || 3000;
 
-app.get("/", function(req,res) {
-	res.sendFile(path.join(__dirname,"..","dist/index.html"),{}, function(err){
-		if (err)
-			console.log(err);
-	});
-});
+app.use(express.static('dist'))
 
 app.listen(port, function(err){
 	console.log("Server is Running on Port " + port);
